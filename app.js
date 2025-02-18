496c504c05d6829a4e97bc13e5c70d5e262ad399
Jwe891116 <2005112848.ub.edu.bz>
2025-02-18 10:28:04 -0600

    update

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-const express = require('express');
+// const express = require('express');
+
+
+import express from 'express';
+import signupRoutesfrom './routes/signupRoutes.js';
 const app = express();
 
-//route to home page
-//headers automatically set and written
-app.get('/', (req, res) => {
-    res.send('Home page.\n');
+//middleware
+
+app.use(express.urlencoded ({extended: true}));
+
+const loggingMiddleware = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+  };
+  
+  app.use(loggingMiddleware);
+  // Use the signup routes
+  app.use('/', signupRoutes);
+  
+  app.use((req, res) => {
+       res.status(404).send('Oops! The page you are looking for does not exist.');
+  });
+  
+
+app.use((req, res, next) => {
+    const timestamp= new Date().toISOString();
+    console.log(`[${timestamp}] ${req.method} ${req.url}`);
+    next();
 });
 
-//route for about page
-//headers automatically set and written
-app.get('/about', (req, res) => {
-    res.send('What do you want to know?\n');
-}); 
-
-app.get('/me', (req, res) => {
-    res.send('My name is Joshua Emmanuel.\n');
-});
-
-app.get('/class', (req, res) => {
-    res.send('GUI Programming.\n');
-});
-
-app.get('/school', (req, res) => {
-    res.send('University of Belize./n');
-});
+const adminMiddleware = (req, res, next) => {
+    if (req.headers["x-admin"] === "true") {
+        console.log("Access granted to /pay\n.");
+        next();
+    }
+    else {
+        console.log("Access denied to /pay.");
+        res.status(403).send("403 Forbidden: Admin access required.\n");
+    }
+};
 
 //catch-all route for 404 errors. our first taste of middleware
 app.use((req, res) => {
@@ -35,3 +50,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
+
--- basics.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const hi = () => console.log("HOLA");
-//hi();
-
-const greetUser= (user) => {
-    return console.log("Hi " + user + "!");
-}
-greetUser("Joshua");
